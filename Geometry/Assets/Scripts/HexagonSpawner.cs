adba63eac5b3b77af3ca9ef7883e4b92c8f98e94
Zaveryuha_Ivan <IZaveryukha@croc.ru>
2021-07-22 21:22:18 +0300

    Refactored 'HexagomSpawner'

---
+++ Geometry/Assets/Scripts/HexagonSpawner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class HexagonSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _hexagon;
+    [SerializeField] private float _spawnRate;
+
+    private float _counter;
+
+    private void Start()
+    {
+        InitializeFields();
+    }
+
+    private void InitializeFields()
+    {
+        _spawnRate = 1f;
+        _counter = 0.0f;
+    }
+
+    void Update()
+    {
+        Countdown();
+
+        if (CounterCountedDownSpawnTime())
+        {
+            SpawnHexagon();
+            _counter = 0f;
+        }
+    }
+
+    private void Countdown()
+    {
+        _counter += Time.deltaTime;
+    }
+
+    private bool CounterCountedDownSpawnTime()
+    {
+        return _counter >= _spawnRate;
+    }
+
+    private GameObject SpawnHexagon()
+    {
+        return (GameObject)Instantiate(_hexagon, transform.position, Quaternion.identity);
+    }
+}
